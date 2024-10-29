604a2383abbd4987d1f09693e45e2e5f8dbbd1f3
Tetoshe <imfiight59@gmail.com>
2024-10-29 11:10:19 +0100

    Add cors

--- Back/index.js
+++ Back/index.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const cors = require('cors')
 
 const app = express()
 const port = 3000
+
+const corsOptions = {
+    origin: "http://localhost:5173",
+    optionsSuccessStatus: 200
+}
+
+app.use(cors(corsOptions))
 
 app.get('/', async (req, res) => {
 
