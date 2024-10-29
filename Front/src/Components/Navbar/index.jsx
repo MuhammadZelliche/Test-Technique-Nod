0f215da044230138608ea9d821bd94cffeb601b5
Tetoshe <imfiight59@gmail.com>
2024-10-29 11:37:53 +0100

    Fix Navbar css

--- Front/src/Components/Navbar/index.jsx
+++ Front/src/Components/Navbar/index.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import './index.scss'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 
 function Navbar() {
+
+    const location = useLocation()
 
     const routes = [{ path: "/", label: "Liste des persos" }, { path: "/characComparator", label: "Comparateur" }, { path: "/characInfos", label: "Infos perso" }]
     return (
         <div className='navContainer'>
             {routes.map((route) => (
-                <Link key={route.path} className='navLink' to={route.path}>{route.label}</Link>
+                <Link style={{ backgroundColor: route.path == location.pathname && "white", color: route.path == location.pathname && 'black' }} key={route.path} className='navLink' to={route.path}>{route.label}</Link>
             ))}
         </div>
     )
